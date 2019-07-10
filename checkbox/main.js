a8fb4e526a12693d5c13b23bb45190802418ffb7
yang ping <yangping@bothub.com>
2019-07-10 15:47:06 +0800

    test checkbox

--- checkbox/main.js
+++ checkbox/main.js
@@ -1,11 +1,11 @@
 createVueComponent({
     id: 'bh-widget-example-checkbox',
     type: 'Checkbox',
-    pageId: 418140585694838,
+    pageId: 491227725017609,
     skin: 'light',
     size: 'large',
     centerAlign: false,
     hideAfterChecked: -1,
-    origin: 'https://uniqueapple123.github.io/',
+    origin: 'https://uniqueapple123.github.io',
     allowLogin: true,
 });
